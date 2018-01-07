8208202105becbaa736366183fd4c27921326196
H.J. Lu <hjl.tools@gmail.com>
2018-01-06 22:29:56 -0800

    x86: Add 'V' register operand modifier
    Add 'V', a special modifier which prints the name of the full integer
    register without '%'.  For
    
    extern void (*func_p) (void);
    
    void
    foo (void)
    {
      asm ("call __x86_indirect_thunk_%V0" : : "a" (func_p));
    }
    
    it generates:
    
    foo:
    	movq	func_p(%rip), %rax
    	call	__x86_indirect_thunk_rax
    	ret
    
    gcc/
    
    	Backport from mainline
    	2018-01-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/i386/i386.c (print_reg): Print the name of the full
    	integer register without '%'.
    	(ix86_print_operand): Handle 'V'.
    	 * doc/extend.texi: Document 'V' modifier.
    
    gcc/testsuite/
    
    	Backport from mainline
    	2018-01-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gcc.target/i386/indirect-thunk-register-4.c: New test.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -15628,6 +15628,7 @@ put_condition_code (enum rtx_code code, machine_mode mode, bool reverse,
    If CODE is 'h', pretend the reg is the 'high' byte register.
    If CODE is 'y', print "st(0)" instead of "st", if the reg is stack op.
    If CODE is 'd', duplicate the operand for AVX instruction.
+   If CODE is 'V', print naked full integer register name without %.
  */
 
 void
@@ -15637,7 +15638,7 @@ print_reg (rtx x, int code, FILE *file)
   unsigned int regno;
   bool duplicated = code == 'd' && TARGET_AVX;
 
-  if (ASSEMBLER_DIALECT == ASM_ATT)
+  if (ASSEMBLER_DIALECT == ASM_ATT && code != 'V')
     putc ('%', file);
 
   if (x == pc_rtx)
@@ -15672,6 +15673,13 @@ print_reg (rtx x, int code, FILE *file)
     code = 32;
   else if (code == 'g')
     code = 64;
+  else if (code == 'V')
+    {
+      if (GENERAL_REGNO_P (regno))
+	code = GET_MODE_SIZE (word_mode);
+      else
+	error ("'V' modifier on non-integer register");
+    }
   else
     code = GET_MODE_SIZE (GET_MODE (x));
 
@@ -15803,6 +15811,7 @@ print_reg (rtx x, int code, FILE *file)
    & -- print some in-use local-dynamic symbol name.
    H -- print a memory address offset by 8; used for sse high-parts
    Y -- print condition for XOP pcom* instruction.
+   V -- print naked full integer register name without %.
    + -- print a branch hint as 'cs' or 'ds' prefix
    ; -- print a semicolon (after prefixes due to bug in older gas).
    ~ -- print "i" if TARGET_AVX2, "f" otherwise.
@@ -16027,6 +16036,7 @@ ix86_print_operand (FILE *file, rtx x, int code)
 	case 'X':
 	case 'P':
 	case 'p':
+	case 'V':
 	  break;
 
 	case 's':
