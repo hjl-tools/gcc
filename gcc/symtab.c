face78adc151ac8f8f21a6564ccf79453cca0376
H.J. Lu <hjl.tools@gmail.com>
2018-04-11 12:31:21 -0700

    Don't mark IFUNC resolver as only called directly
    Since IFUNC resolver is called indirectly, don't mark IFUNC resolver as
    only called directly.  This patch adds ifunc_resolver to cgraph_node,
    sets ifunc_resolver for ifunc attribute and checks ifunc_resolver
    instead of looking up ifunc attribute.
    
    gcc/
    
    	PR target/85345
    	* cgraph.h (cgraph_node::create): Set ifunc_resolver for ifunc
    	attribute.
    	(cgraph_node::create_alias): Likewise.
    	(cgraph_node::get_availability): Check ifunc_resolver instead
    	of looking up ifunc attribute.
    	* cgraphunit.c (maybe_diag_incompatible_alias): Likewise.
    	* symtab.c (symtab_node::binds_to_current_def_p): Likewise.
    	* varasm.c (do_assemble_alias): Likewise.
    	(assemble_alias): Likewise.
    	(default_binds_local_p_3): Likewise.
    	* cgraph.h (cgraph_node): Add ifunc_resolver.
    	(cgraph_node::only_called_directly_or_aliased_p): Return false
    	for IFUNC resolver.
    	* lto-cgraph.c (input_node): Set ifunc_resolver for ifunc
    	attribute.
    
    gcc/testsuite/
    
    	PR target/85345
    	* gcc.target/i386/pr85345.c: New test.

--- gcc/symtab.c
+++ gcc/symtab.c
@@ -2247,13 +2247,13 @@ symtab_node::binds_to_current_def_p (symtab_node *ref)
   if (transparent_alias)
     return definition
 	   && get_alias_target()->binds_to_current_def_p (ref);
-  if (lookup_attribute ("ifunc", DECL_ATTRIBUTES (decl)))
+  cgraph_node *cnode = dyn_cast <cgraph_node *> (this);
+  if (cnode && cnode->ifunc_resolver)
     return false;
   if (decl_binds_to_current_def_p (decl))
     return true;
 
   /* Inline clones always binds locally.  */
-  cgraph_node *cnode = dyn_cast <cgraph_node *> (this);
   if (cnode && cnode->global.inlined_to)
     return true;
 
