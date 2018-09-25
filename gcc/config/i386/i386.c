f6419afaadacd6628d81237070d687ca0cf361d5
H.J. Lu <hjl.tools@gmail.com>
2018-09-25 06:34:17 -0700

    i386: Check -mindirect-branch/-mfunction-return with -fcf-protection
    Issue an error when -mindirect-branch or -mfunction-return are used
    with incompatible -fcf-protection.
    
    gcc/
    
    	PR target/87412
    	* config/i386/i386.c (ix86_set_indirect_branch_type): Issue an
    	error for -mindirect-branch/-mfunction-return with incompatible
    	-fcf-protection.
    
    gcc/testsuite/
    
    	PR target/87412
    	* gcc.target/i386/pr87412-1.c: New file.
    	* gcc.target/i386/pr87412-2.c: Likewise.
    	* gcc.target/i386/pr87412-3.c: Likewise.
    	* gcc.target/i386/pr87412-4.c: Likewise.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -5600,6 +5600,11 @@ ix86_set_indirect_branch_type (tree fndecl)
 	       ((cfun->machine->indirect_branch_type
 		 == indirect_branch_thunk_extern)
 		? "thunk-extern" : "thunk"));
+
+      if (cfun->machine->indirect_branch_type != indirect_branch_keep
+	  && (flag_cf_protection & CF_RETURN))
+	error ("%<-mindirect-branch%> and %<-fcf-protection%> are not "
+	       "compatible");
     }
 
   if (cfun->machine->function_return_type == indirect_branch_unset)
@@ -5638,6 +5643,11 @@ ix86_set_indirect_branch_type (tree fndecl)
 	       ((cfun->machine->function_return_type
 		 == indirect_branch_thunk_extern)
 		? "thunk-extern" : "thunk"));
+
+      if (cfun->machine->function_return_type != indirect_branch_keep
+	  && (flag_cf_protection & CF_RETURN))
+	error ("%<-mfunction-return%> and %<-fcf-protection%> are not "
+	       "compatible");
     }
 }
 
