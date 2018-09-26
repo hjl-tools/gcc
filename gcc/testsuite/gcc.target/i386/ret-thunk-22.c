71c96e824a737718a56c39be5d01519e0920188f
H.J. Lu <hjl.tools@gmail.com>
2018-09-26 11:07:01 -0700

    Update gcc.target/i386/ret-thunk-2[234].c

--- gcc/testsuite/gcc.target/i386/ret-thunk-22.c
+++ gcc/testsuite/gcc.target/i386/ret-thunk-22.c
@@ -7,7 +7,7 @@ struct s gs = { 100 + 200i };
 struct s __attribute__((noinline)) foo (void) { return gs; }
 
 /* { dg-final { scan-assembler-times "popl\[\\t \]*%ecx" 1 } } */
-/* { dg-final { scan-assembler "lea\[l\]?\[\\t \]*4\\(%esp\\), %esp" } } */
+/* { dg-final { scan-assembler-times "popl\[\\t \]*%edx" 1 } } */
 /* { dg-final { scan-assembler "jmp\[ \t\]*__x86_return_thunk_ecx" } } */
 /* { dg-final { scan-assembler "jmp\[ \t\]*\.LIND" } } */
 /* { dg-final { scan-assembler "call\[ \t\]*\.LIND" } } */
