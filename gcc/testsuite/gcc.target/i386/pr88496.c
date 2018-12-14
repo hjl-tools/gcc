053047212ea685c62149027b7c419dfe0cb71014
H.J. Lu <hjl.tools@gmail.com>
2018-12-14 12:36:54 -0800

    x86: Set frame size to 0 if stack frame isn't required
    get_frame_size () returns used stack slots during compilation, which
    may be optimized out later.  Since ix86_find_max_used_stack_alignment
    checks if stack frame is required, it can set a bit in machine_function
    to let ix86_compute_frame_layout know that stack frame isn't required.
    
    Tested on i686 and x86-64 with
    
    --with-arch=native --with-cpu=native
    
    on AVX512 machine.  Tested on i686 and x86-64 without
    
    --with-arch=native --with-cpu=native
    
    on x86-64 machine.
    
    gcc/
    
    	PR target/88496
    	* config/i386/i386.c (ix86_compute_frame_layout): Set frame size
    	to 0 if the function doesn't need a stack frame.
    	(ix86_find_max_used_stack_alignment): Set no_stack_frame to 1 if
    	stack frame isn't required.
    	* config/i386/i386.h (machine_function): Add no_stack_frame.
    
    gcc/testsuite/
    
    	PR target/88496
    	* gcc.target/i386/pr88496.c: New test.

---
+++ gcc/testsuite/gcc.target/i386/pr88496.c
@@ -0,0 +1,17 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mavx512f" } */
+
+struct B
+{
+  char a[12];
+  int b;
+};
+
+struct B
+f2 (void)
+{
+  struct B x = {};
+  return x;
+}
+
+/* { dg-final { scan-assembler-not "(sub|add)(l|q)\[\\t \]*\\$\[0-9\]*,\[\\t \]*%\[re\]?sp" } } */
